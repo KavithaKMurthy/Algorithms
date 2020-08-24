a57214119cb18a8e5abd842532d9f49fb37d46a9
kavithakmurthy <kavitha.kmurthy10@gmail.com>
2020-08-24 22:53:05 +0530

    Algorithms

--- src/main/java/NoRepeatSubstring.java
+++ src/main/java/NoRepeatSubstring.java
@@ -4,18 +4,18 @@ import java.util.Map;
 public class NoRepeatSubstring {
 
     public static int findLength(String str) {
-        int windowStart = 0, maxLength = 0;
-        Map<Character, Integer> charIndexMap = new HashMap<>();
-       for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
-            char rightChar = str.charAt(windowEnd);
-            if (charIndexMap.containsKey(rightChar)) {
-                windowStart = Math.max(windowStart, charIndexMap.get(rightChar) + 1);
-            }
-            charIndexMap.put(rightChar, windowEnd);
-            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
-        }
-
-        return maxLength;
+       int windowStart =0 , maxLength = 0;
+       Map<Character, Integer> characterMap = new HashMap<>();
+       for(int windowEnd = 0; windowEnd <= str.length()-1; windowEnd++)
+       {
+           char rightChar = str.charAt(windowEnd);
+           if(characterMap.containsKey(rightChar)){
+               windowStart = Math.max(windowStart,characterMap.get(rightChar) +1 );
+           }
+           characterMap.put(rightChar, windowEnd);
+           maxLength = Math.max(maxLength, windowEnd-windowStart +1);
+       }
+       return maxLength;
     }
 
         public static void main(String[] args) {
